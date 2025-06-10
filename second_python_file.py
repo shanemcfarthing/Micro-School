5fafe98ad02323da751d8a709fcdd8df9aa05065
shanemcfarthing <shane.mcfarthing@gmail.com>
2025-06-10 21:04:46 +0200

    Added a second simple python function to test the tab configuration for indentation

---
+++ second_python_file.py
@@ -0,0 +1,9 @@
+"""
+This module contains a second function, with the purpose of testing that
+the tabs in VS Code have been configured as 4 spaces.
+"""
+
+def second_simple_function():
+
+    print("Inside the second simple function")
+    
