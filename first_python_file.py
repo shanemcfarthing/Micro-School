53bc0c5f6f41abef218190300a67afd6a94b3be2
shanemcfarthing <shane.mcfarthing@gmail.com>
2025-06-10 20:59:21 +0200

    Added final newline to the module.

--- first_python_file.py
+++ first_python_file.py
@@ -7,3 +7,4 @@ def simple_function(input_1: int, input_2: int):
     A function to demonstrate a simple addition operation.
     """
     print(f"Sum of two input numbers {input_1} and {input_2}: {input_1+input_2}")
+    
