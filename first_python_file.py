cc736e1bf3a965ac650080b9b59985faf92e8c93
shanemcfarthing <shane.mcfarthing@gmail.com>
2025-06-10 20:55:17 +0200

    Updated the indentation and docstring for the simple function.

--- first_python_file.py
+++ first_python_file.py
@@ -1,5 +1,6 @@
-def simple_function(input_1: int,
-		    input_2: int):
-
+def simple_function(input_1: int, input_2: int):
+	"""
+	A function to demonstrate a simple addition operation.
+	"""
 	print(f"Sum of two input numbers {input_1} and {input_2}: {input_1+input_2}")
 
